89111aee99e83a5048df2f5089fc7b885ae65d45
f3lixh4wk <86167136+f3lixh4wk@users.noreply.github.com>
2021-10-22 11:42:49 +0200

    Uebung1 Verbesserungen & Uebung3 hinzugefügt

---
+++ Uebung_1_Verbesserung/Uebung_1_Verbesserung/Aufgabe1-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aufgabe_1_1
+{
+   class Program
+   {
+      static void Main()
+      {
+         Testklasse test = new Testklasse();
+
+         try
+         {
+            test.Test = 10;
+            Console.WriteLine("test besitzt den Wert: {0}", test.Test);
+            test.Test = 101;
+         }
+         catch (Exception exec)
+         {
+            Console.WriteLine(exec);
+         }
+      }
+   }
+
+   class Testklasse
+   {
+      int test;
+      public int Test
+      {
+         set
+         {
+            if (value < 0 || value > 100)
+               throw new ArgumentOutOfRangeException("Wert nicht zulässig");
+            else
+               test = value;
+         }
+         get
+         { return test; }
+      }
+   }
+}
